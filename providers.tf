e136630fc5126e0806a4ba2a5187ec0cce7a84fe
Lucas Barros <50954822+LucasBarrosDosSantos@users.noreply.github.com>
2022-06-24 19:01:19 -0300

    fix name project

--- providers.tf
+++ providers.tf
@@ -12,7 +12,7 @@ provider "google" {
 
   credentials = file("/home/vagrant/gcpkey.json")
 
-  project = "DEFINIR"
+  project = "devops-fiap-lab"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
