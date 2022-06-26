92518c01ce990dc0db0420d965058f2c6b830c0b
Lucas Barros <50954822+LucasBarrosDosSantos@users.noreply.github.com>
2022-06-26 16:44:06 -0300

    GCP App Engine config

--- providers.tf
+++ providers.tf
@@ -10,7 +10,12 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
+  project = "devops-fiap-lab"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
 
+provider "google-beta" {
   project = "devops-fiap-lab"
   region  = "us-west1"
   zone    = "us-west1-b"
